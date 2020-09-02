e8abec2b2d6777505f5b1287e7790be68ac7b79a
FreekAlberti <federicoalberi96@gmail.com>
2020-09-02 17:18:27 +0200

    keydown

--- main.js
+++ main.js
@@ -2,25 +2,43 @@ $("document").ready(function () {
 
   $(".next > i").click(
     function() {
-      var images = $(".images > img.active");
-      if (images.hasClass("last")) {
-        $(".images > img.first").addClass("active");
-      } else {
-        images.next().addClass("active");
-      }
-      images.removeClass("active");
+      myNext();
     }
   );
 
   $(".prev > i").click(
     function() {
-      var images = $(".images > img.active");
-      if (images.hasClass("first")) {
-        $(".images > img.last").addClass("active");
-      } else {
-        images.prev().addClass("active");
-      }
-      images.removeClass("active");
+      myPrev();
     }
   );
-});
+
+  $(document).keydown(function(e){
+    if (e.keyCode == 39) {
+      myNext();
+    } else if (e.keyCode == 37) {
+      myPrev();
+    }
+  });
+})
+
+//FUNCTION
+
+function myNext() {
+  var images = $(".images > img.active");
+  if (images.hasClass("last")) {
+    $(".images > img.first").addClass("active");
+  } else {
+    images.next().addClass("active");
+  }
+  images.removeClass("active");
+}
+
+function myPrev() {
+  var images = $(".images > img.active");
+  if (images.hasClass("first")) {
+    $(".images > img.last").addClass("active");
+  } else {
+    images.prev().addClass("active");
+  }
+  images.removeClass("active");
+}
