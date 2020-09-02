d573c53a1bfd1d575755d6e6469abccdf76e7cbd
FreekAlberti <federicoalberi96@gmail.com>
2020-09-02 16:18:00 +0200

    immagini scorrono next/prev

--- main.js
+++ main.js
@@ -2,13 +2,25 @@ $("document").ready(function () {
 
   $(".next > i").click(
     function() {
-      $(".images > img.active").removeClass("active");
+      var images = $(".images > img.active");
+      if (images.hasClass("last")) {
+        $(".images > img.first").addClass("active");
+      } else {
+        images.next().addClass("active");
+      }
+      images.removeClass("active");
     }
   );
 
   $(".prev > i").click(
     function() {
-    $(".images > img.active").removeClass("active");
-  }
+      var images = $(".images > img.active");
+      if (images.hasClass("first")) {
+        $(".images > img.last").addClass("active");
+      } else {
+        images.prev().addClass("active");
+      }
+      images.removeClass("active");
+    }
   );
 });
