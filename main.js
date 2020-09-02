e10ea7e9d0691d72422c5cdd3a75905be3b7e510
FreekAlberti <federicoalberi96@gmail.com>
2020-09-02 17:24:48 +0200

    pallini scorrono

--- main.js
+++ main.js
@@ -25,20 +25,34 @@ $("document").ready(function () {
 
 function myNext() {
   var images = $(".images > img.active");
+  var circle = $(".nav > i.active");
   if (images.hasClass("last")) {
     $(".images > img.first").addClass("active");
   } else {
     images.next().addClass("active");
   }
   images.removeClass("active");
+  if (circle.hasClass("last")) {
+    $(".nav > i.first").addClass("active");
+  } else {
+    circle.next().addClass("active");
+  }
+  circle.removeClass("active");
 }
 
 function myPrev() {
   var images = $(".images > img.active");
+  var circle = $(".nav > i.active");
   if (images.hasClass("first")) {
     $(".images > img.last").addClass("active");
   } else {
     images.prev().addClass("active");
   }
   images.removeClass("active");
+  if (circle.hasClass("first")) {
+    $(".nav > i.last").addClass("active");
+  } else {
+    circle.prev().addClass("active");
+  }
+  circle.removeClass("active");
 }
